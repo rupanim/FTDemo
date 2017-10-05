a8d5c0cd9633c1b58a1a0f52870dc4f6957aabcd
Mohit Rupani <mohit_rupani@azimuthsystems.com>
2017-10-05 16:59:06 -0400

    Files Added - First Time

---
+++ FTDemo/app/Factory/factory.js
@@ -0,0 +1,45 @@
+﻿
+
+    app.factory('AbbrvList', [
+        function () {
+            return function () {
+                var abbrvList = ['MA', 'MI', 'CA', 'RI', 'PA'];
+                return abbrvList;
+            }
+        }
+    ]);
+
+    app.factory('DefinitionList', [
+        function () {
+            return function () {
+                var definitionList = ['Massachusetts', 'Michigan', 'California', 'Rhode Island', 'Pennsylvania'];
+                return definitionList;
+            }
+        }
+    ]);
+
+    app.factory('MatchFunc', [
+        function () {
+            return function (abbrv, defintion) {
+
+                var matched = false;
+
+                var matchList = [{ key: "MA", value: "Massachusetts" },
+                { key: "MI", value: "Michigan" },
+                { key: "CA", value: "California" },
+                { key: "RI", value: "Rhode Island" },
+                { key: "PA", value: "Pennsylvania" }];
+
+                for (var index = 0; index < matchList.length; ++index) {
+                    if (matchList[index].key == abbrv) {
+                        if (matchList[index].value == defintion) {
+                            matched = true;
+                            break;
+                        }
+                    }
+                }
+                return matched;
+            }
+        }
+    ]);
+
---
+++ FTDemo/app/app.js
@@ -0,0 +1,24 @@
+﻿
+    var app = new angular.module('app', ['ngMaterial', 'ngRoute']);
+
+app.config(function ($routeProvider) {
+        $routeProvider
+            .when("/", {
+                templateUrl: "<h1>Banana<h1>"
+        }) 
+        .when("Main.html", {
+            templateUrl: "<h1>Main.html<h1>"
+        }) 
+        .when("/Main.Html", {
+            templateUrl: "<h1>Slash Main.html<h1>"
+        })
+            .otherwise({
+                templateUrl: "<h1>Otherwise<h1>"
+            })
+           
+    });
+
+   
+
+  
+
