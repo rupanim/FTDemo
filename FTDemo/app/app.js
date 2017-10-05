c193c7ca2cd8f5a834f76bfb67932b8942333b3d
Mohit Rupani <mohit_rupani@azimuthsystems.com>
2017-10-05 17:07:25 -0400

    Cleanup

--- FTDemo/app/app.js
+++ FTDemo/app/app.js
@@ -1,22 +1,5 @@
 ﻿
-    var app = new angular.module('app', ['ngMaterial', 'ngRoute']);
-
-app.config(function ($routeProvider) {
-        $routeProvider
-            .when("/", {
-                templateUrl: "<h1>Banana<h1>"
-        }) 
-        .when("Main.html", {
-            templateUrl: "<h1>Main.html<h1>"
-        }) 
-        .when("/Main.Html", {
-            templateUrl: "<h1>Slash Main.html<h1>"
-        })
-            .otherwise({
-                templateUrl: "<h1>Otherwise<h1>"
-            })
-           
-    });
+    var app = new angular.module('app', ['ngMaterial']);
 
    
 
